f28a14b2e92d0688c2dfe5a52a731a46d5cbc852
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-01-09 01:01:23 +0900

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,5 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", event => {
+	MsDGo7Eg.style.display = "none";
+	m7eOG5wP.style.animation = "none";
+}, false);
---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+const {keyword = ""} = getParameter();
+const ref = database.ref("article");
+keyword === "" ? ref.limitToLast(4).once("value", snapshot => {
+	const val = snapshot.val();
+	if (val !== null) Object.keys(val).forEach(createListItem, val);
+	MsDGo7Eg.style.display = "none";
+	m7eOG5wP.style.animation = "none";
+}) : ref.once("value", snapshot => {
+	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`;
+	const val = snapshot.val();
+	if (val !== null) Object.keys(val).filter(key => String(val[key].title).match(new RegExp(keyword))).forEach(createListItem, val);
+	MsDGo7Eg.style.display = "none";
+	m7eOG5wP.style.animation = "none";
+});
