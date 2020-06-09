d82d439e401340683497cc20da61c1aff0955091
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-06-09 16:15:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ keyword === "" ? ref.limitToLast(4).once("value", snapshot => {
 }) : ref.once("value", snapshot => {
 	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`;
 	const val = snapshot.val();
-	if (val !== null) Object.values(val).filter(value => {
-		console.log(value);
-		return String(value.title).includes(keyword);
-	}).forEach(createListItem, val);
+	if (val !== null) Object.keys(val).filter(key => String(val[key].title).includes(keyword)).forEach(createListItem, key);
 	MsDGo7Eg.style.display = "none";
 	m7eOG5wP.style.animation = "none";
 });
