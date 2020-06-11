b4181d0991a3ed5e3f227c2748700cd00b84b1cc
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-06-11 15:02:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 const {keyword = ""} = getParameter();
 const ref = database.ref("article");
-(() => keyword === "" ? (function() {
+(() => keyword === "" ? ref : (function() {
 	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`;
 	return ref.limitToLast(4);
-})() : ref)().once("value", snapshot => {
+})())().once("value", snapshot => {
 	const val = snapshot.val();
 	if (val !== null) Object.keys(val).filter(key => keyword === "" || String(val[key].title).includes(keyword)).forEach(createListItem, val);
 	MsDGo7Eg.style.display = "none";
