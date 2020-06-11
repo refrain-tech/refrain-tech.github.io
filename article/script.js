3bd6a9161afa8becf0fedbddfc04e1a3b69f4aa1
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-06-11 15:26:04 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -1,5 +1,23 @@
 "use strict";
 const {category = "", key = ""} = getParameter();
+category === "" ? location.href = "/" : (hasKey => {
+	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${([["articles", "記事"], ["contents", "コンテンツ"], ["service", "サービス"]].find(element => element[0] === category) || ["", "その他"])[1]}</a></li>`;
+	(hasKey ? database.ref(`article/${key}`) : database.ref("article").orderByChild("category").equalTo(category).limitToLast(4)).once("value", snapshot => {
+		const val = snapshot.val() ?? {};
+		hasKey ? (() => {
+			const {content = "", title = ""} = val;
+			if (content !== "" && title !== "") {
+				document.title = title;
+				YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}&key=${key}">${title}</a></li>`;
+				FE9UGRQR.innerHTML = `<section id = "WSeaG6gm">${content}</section>`;
+			}
+		})() : (() => Object.keys(val).forEach(createListItem, val))();
+		MsDGo7Eg.style.display = "none";
+		m7eOG5wP.style.animation = "none";
+	});
+})(key !== "");
+/*
+const {category = "", key = ""} = getParameter();
 if (category === "") location.href = "/";
 else {
 	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${category === "articles" ? "記事" : category === "contents" ? "コンテンツ" : category === "service" ? "サービス" : "その他"}</a></li>`;
@@ -20,3 +38,4 @@ else {
 	MsDGo7Eg.style.display = "none";
 	m7eOG5wP.style.animation = "none";
 }
+*/
