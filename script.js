e3a6bf322ab0da3922603c01eb925e40bbb67a77
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-06-21 21:57:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-"use strict";
-const {keyword = ""} = getParameter();
-let ref = database.ref("article");
-if (keyword === "") ref = ref.limitToLast(4);
-else YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`
-ref.once("value", snapshot => {
-	const val = snapshot.val() ?? {};
-	Object.keys(val).filter(key => keyword === "" || String(val[key].title).includes(keyword)).forEach(createListItem, val);
-	MsDGo7Eg.style.display = "none";
-	m7eOG5wP.style.animation = "none";
-});
+(function() {
+	"use strict";
+	const {keyword = ""} = getParameter();
+	let ref = database.ref("article");
+	if (keyword === "") ref = ref.limitToLast(4);
+	else YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`
+	ref.once("value", snapshot => {
+		const val = snapshot.val() ?? {};
+		Object.keys(val).filter(key => keyword === "" || String(val[key].title).includes(keyword)).forEach(createListItem, val);
+		MsDGo7Eg.style.display = "none";
+		m7eOG5wP.style.animation = "none";
+	});
+})();
