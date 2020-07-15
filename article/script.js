5dac7cea214644812a05f35a64624b176c7a16e5
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-15 12:56:08 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -1,21 +1,23 @@
-"use strict";
-const { category = "", key = "" } = getParameter();
-const currentCategory = categories.find(currentValue => currentValue[0] === category);
-if (currentCategory === undefined) return location.href = "/";
-YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${(currentCategory || ["", "その他"])[1]}</a></li>`;
-const ref = key === "" ? database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) : database.ref(`article/${key}`);
-ref.once("value", snapshot => {
-	const val = snapshot.val() ?? {};
-	if (key === "") Object.keys(val).forEach(createListItem, val);
-	else {
-		const { content = "", title = "" } = val;
-		if (content === "" || title === "") location.href = "/";
+(function() {
+	"use strict";
+	const { category = "", key = "" } = getParameter();
+	const currentCategory = categories.find(currentValue => currentValue[0] === category);
+	if (currentCategory === undefined) return location.href = "/";
+	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${(currentCategory || ["", "その他"])[1]}</a></li>`;
+	const ref = key === "" ? database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) : database.ref(`article/${key}`);
+	ref.once("value", snapshot => {
+		const val = snapshot.val() ?? {};
+		if (key === "") Object.keys(val).forEach(createListItem, val);
 		else {
-			document.title = title;
-			YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}&key=${key}">${title}</a></li>`;
-			FE9UGRQR.innerHTML = `<section id = "WSeaG6gm">${content}</section>`;
+			const { content = "", title = "" } = val;
+			if (content === "" || title === "") location.href = "/";
+			else {
+				document.title = title;
+				YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}&key=${key}">${title}</a></li>`;
+				FE9UGRQR.innerHTML = `<section id = "WSeaG6gm">${content}</section>`;
+			}
 		}
-	}
-	MsDGo7Eg.style.display = "none";
-	m7eOG5wP.style.animation = "none";
-});
+		MsDGo7Eg.style.display = "none";
+		m7eOG5wP.style.animation = "none";
+	});
+})();
