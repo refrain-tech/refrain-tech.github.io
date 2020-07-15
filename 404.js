f6cb120eb032592acf7d8ace1fcde0fb0a55dccb
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-15 12:57:09 +0900

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,5 +1,7 @@
-"use strict";
-document.addEventListener("DOMContentLoaded", event => {
-	MsDGo7Eg.style.display = "none";
-	m7eOG5wP.style.animation = "none";
-}, false);
+(function() {
+	"use strict";
+	document.addEventListener("DOMContentLoaded", event => {
+		MsDGo7Eg.style.display = "none";
+		m7eOG5wP.style.animation = "none";
+	}, false);
+})();
