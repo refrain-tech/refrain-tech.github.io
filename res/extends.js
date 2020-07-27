9c4837e1e9dc310b0903cb723f6c24f9b450ec80
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-27 19:21:55 +0900

    Update extends.js

--- res/extends.js
+++ res/extends.js
@@ -5,6 +5,9 @@ Array.prototype.shuffle = function() {
 		[this[index], this[random]] = [this[random], this[index]];
 	}
 };
+HTMLCollection.prototype.filter = function(callback) { [].filter.call(this, callback) };
+HTMLCollection.prototype.forEach = function(callback) { [].forEach.call(this, callback) };
+HTMLCollection.prototype.map = function(callback) { [].map.call(this, callback) };
 HTMLElement.prototype.copy = function() {
 	const range = document.createRange();
 	range.selectNode(this);
@@ -14,22 +17,20 @@ HTMLElement.prototype.copy = function() {
 	document.execCommand("copy");
 	selection.removeAllRanges();
 };
-HTMLElement.prototype.prependChild = function(element) {
-	this.insertBefore(element, this.firstChild);
-};
-HTMLSelectElement.prototype.getSelection = function() {
-	return this.options[this.selectedIndex];
-};
-String.prototype.pattern = function(regexp) {
-	return this.replace(regexp, "") === "";
-};
-String.prototype.replaceAll = function(regexp, newSubstr) {
-	return this.replace(new RegExp(regexp, "g"), newSubstr);
-};
+HTMLElement.prototype.prependChild = function(element) { this.insertBefore(element, this.firstChild) };
+HTMLSelectElement.prototype.getSelection = function() { return this.options[this.selectedIndex] };
+NodeList.prototype.filter = function(callback) { [].filter.call(this, callback) };
+NodeList.prototype.forEach = function(callback) { [].forEach.call(this, callback) };
+NodeList.prototype.map = function(callback) { [].map.call(this, callback) };
+String.prototype.pattern = function(regexp) { return this.replace(regexp, "") === "" };
+String.prototype.replaceAll = function(regexp, newSubstr) { return this.replace(new RegExp(regexp, "g"), newSubstr) };
 window.isNumber = value => typeof value === "number" && isFinite(value);
-XMLHttpRequest.requestSync = function(url) {
-	const request = new XMLHttpRequest();
-	request.open("GET", url, false);
-	request.send(null);
-	return request;
+XMLHttpRequest.requestSync = async function(url) {
+	return new Promise(function(resolve, reject) {
+		const request = new XMLHttpRequest();
+		request.open("GET", url);
+		request.addEventListener("error", function(event) { reject(this) });
+		request.addEventListener("load", function(event) { resolve(this) });
+		request.send();
+	});
 };
