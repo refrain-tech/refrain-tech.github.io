e8b6ef2dbe9de4cafc26d198d0caf941067bc11c
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-30 20:15:27 +0900

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,10 +1,3 @@
-/**
- * @overview 404.js 404ページの制御スクリプト
- * @since 2020/7/28
- * @version 1.0
- * @author Refrain Tech <refrain.tech@gmail.com>
- * @copyright (c) Copyright 2020 refrain.tech All Rights Reserved
- */
 (() => {
 	"use strict";
 	dismissDialog();
