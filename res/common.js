daa618f33ebc53e4deca69f58b655ae0a8c2c9c3
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-30 20:30:29 +0900

    Update common.js

--- res/common.js
+++ res/common.js
@@ -1,10 +1,3 @@
-/**
- * @overview common.js Github Pages内で共通で使用する関数・変数を実装する
- * @since 2020/7/28
- * @version 1.0
- * @author Refrain Tech <refrain.tech@gmail.com>
- * @copyright (c) Copyright 2020 refrain.tech All Rights Reserved
- */
 "use strict";
 /**
  * @function createListItem Firebase Database上のデータを使用して記事の一覧に追記する
@@ -18,17 +11,6 @@ function createListItem(key) {
 function dismissDialog() {
 	[MsDGo7Eg.style.display, m7eOG5wP.style.animation] = ["none", "none"];
 }
-/**
- * @function getParameter URLのパラメータをオブジェクトに変換する
- * @return {object<string, string>} パラメータをKey: Valueのペアに変換したオブジェクト
- */
-function getParameter() {
-	return location.search.slice(1).split("&").filter(currentValue => /.+=.+/.test(currentValue)).map(currentValue => currentValue.split("=")).reduce((accumulator, currentValue) => {
-		accumulator[currentValue[0]] = currentValue[1];
-		return accumulator;
-	}, {});
-}
-/** @event mWYXHFlu#keydown */
 mWYXHFlu.addEventListener("keydown", event => {
 	switch (event.keyCode) {
 		case 13:
@@ -37,7 +19,6 @@ mWYXHFlu.addEventListener("keydown", event => {
 			break;
 	}
 });
-/** @event SYaLdKxJ#click */
 SYaLdKxJ.addEventListener("click", event => location.href = `/?keyword=${mWYXHFlu.value}`);
 /** @constant {array<array<string>>} categories Github Pagesで表示する記事のカテゴリー */
 const categories = [
