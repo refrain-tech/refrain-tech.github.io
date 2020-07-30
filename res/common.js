8632a3ca0fdba7ac49a38d9cc880211fd4aaac5f
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-07-30 11:51:34 +0900

    Update common.js

--- res/common.js
+++ res/common.js
@@ -1,7 +1,7 @@
 /**
- * @overview Github Pages内で共通で使用する関数・変数を実装する
- * @version 1.0
+ * @overview common.js Github Pages内で共通で使用する関数・変数を実装する
  * @since 2020/7/28
+ * @version 1.0
  * @author Refrain Tech <refrain.tech@gmail.com>
  * @copyright (c) Copyright 2020 refrain.tech All Rights Reserved
  */
@@ -14,9 +14,7 @@ function createListItem(key) {
 	const { category = "other", image = "/res/favicon.png", title = "", url = "" } = this[key];
 	c6JO6k62.innerHTML += `<section class = "W1JhWxuv"><a category = ${category} class = "TVOxHpZ9" href = ${url === "" ? `/article/?category=${category}&key=${key}` : url} target = "_new"><h1 class = "eFcynxxU">${title}</h1><img class = "WaWu7Va9" src = ${image} /></a></section>`;
 }
-/**
- * @function dismissDialog 読み込み中に表示されるダイアログを非表示にする
- */
+/** @function dismissDialog 読み込み中に表示されるダイアログを非表示にする */
 function dismissDialog() {
 	[MsDGo7Eg.style.display, m7eOG5wP.style.animation] = ["none", "none"];
 }
@@ -30,6 +28,7 @@ function getParameter() {
 		return accumulator;
 	}, {});
 }
+/** @event mWYXHFlu#keydown */
 mWYXHFlu.addEventListener("keydown", event => {
 	switch (event.keyCode) {
 		case 13:
@@ -38,10 +37,9 @@ mWYXHFlu.addEventListener("keydown", event => {
 			break;
 	}
 });
+/** @event SYaLdKxJ#click */
 SYaLdKxJ.addEventListener("click", event => location.href = `/?keyword=${mWYXHFlu.value}`);
-/**
- * @constant {array<array<string>>} categories Github Pagesで表示する記事のカテゴリー
- */
+/** @constant {array<array<string>>} categories Github Pagesで表示する記事のカテゴリー */
 const categories = [
 	["article", "記事"],
 	["content", "コンテンツ"],
