94b9389ccded0aba31eea3a585acd0f3077313a8
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-04 22:19:51 +0900

    Update common.js

--- res/common.js
+++ res/common.js
@@ -1,15 +1,10 @@
 "use strict";
-/**
- * @function createListItem Firebase Database上のデータを使用して記事の一覧に追記する
- * @argument {string} key Firebase Datebase上のデータのUID
- */
 function createListItem(key) {
 	const { category = "other", image = "/res/favicon.png", title = "", url = "" } = this[key];
 	c6JO6k62.innerHTML += `<section class = "W1JhWxuv"><a category = ${category} class = "TVOxHpZ9" href = ${url === "" ? `/article/?category=${category}&key=${key}` : url} target = "_new"><h1 class = "eFcynxxU">${title}</h1><img class = "WaWu7Va9" src = ${image} /></a></section>`;
 }
-/** @function dismissDialog 読み込み中に表示されるダイアログを非表示にする */
 function dismissDialog() {
-	[MsDGo7Eg.style.display, m7eOG5wP.style.animation] = ["none", "none"];
+	[ MsDGo7Eg.style.display, m7eOG5wP.style.animation ] = [ "none", "none" ];
 }
 mWYXHFlu.addEventListener("keydown", event => {
 	switch (event.keyCode) {
@@ -20,7 +15,6 @@ mWYXHFlu.addEventListener("keydown", event => {
 	}
 });
 SYaLdKxJ.addEventListener("click", event => location.href = `/?keyword=${mWYXHFlu.value}`);
-/** @constant {array<array<string>>} categories Github Pagesで表示する記事のカテゴリー */
 const categories = [
 	["article", "記事"],
 	["content", "コンテンツ"],
