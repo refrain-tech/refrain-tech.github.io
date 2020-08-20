c2d61cd67bec6f76cfe172ae655aaaee3add7d85
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-20 18:03:57 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -3,9 +3,7 @@ const { category = "", key = "" } = document.getQueryParameters();
 const hasCategory = category in categories;
 if (!hasCategory) location.href = "/";
 YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${categories.category}</a></li>`;
-const ref = key === "" ?
-	database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) :
-	database.ref(`article/${key}`);
+const ref = key === "" ? database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) : database.ref(`article/${key}`);
 ref.once("value", snapshot => {
 	const val = snapshot.val() ?? {};
 	if (key === "") Object.keys(val).forEach(createListItem, val);
