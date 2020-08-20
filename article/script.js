2f7fb903076d135838352551e2a774b54ff9a70d
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-20 17:52:30 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -1,22 +1,22 @@
-(() => {
-	"use strict";
-	const { category = "", key = "" } = document.getQueryParameters();
-	const currentCategory = categories.find(currentValue => currentValue[0] === category);
-	if (currentCategory === undefined) return location.href = "/";
-	YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${currentCategory[1]}</a></li>`;
-	const ref = key === "" ? database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) : database.ref(`article/${key}`);
-	ref.once("value", snapshot => {
-		const val = snapshot.val() ?? {};
-		if (key === "") Object.keys(val).forEach(createListItem, val);
+"use strict";
+const { category = "", key = "" } = document.getQueryParameters();
+const hasCategory = category in categories;
+if (!hasCategory) location.href = "/";
+YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}">${categories.category}</a></li>`;
+const ref = key === "" ?
+	database.ref("article").orderByChild("category").equalTo(category).limitToLast(4) :
+	database.ref(`article/${key}`);
+ref.once("value", snapshot => {
+	const val = snapshot.val() ?? {};
+	if (key === "") Object.keys(val).forEach(createListItem, val);
+	else {
+		const { content = "", title = "" } = val;
+		if (content === "" || title === "") location.href = "/";
 		else {
-			const { content = "", title = "" } = val;
-			if (content === "" || title === "") location.href = "/";
-			else {
-				document.title = title;
-				YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}&key=${key}">${title}</a></li>`;
-				FE9UGRQR.innerHTML = `<section id = "WSeaG6gm">${content}</section>`;
-			}
+			document.title = title;
+			YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/article/?category=${category}&key=${key}">${title}</a></li>`;
+			FE9UGRQR.innerHTML = `<section id = "WSeaG6gm">${content}</section>`;
 		}
-		dismissDialog();
-	});
-})();
+	}
+	dismissDialog();
+});
