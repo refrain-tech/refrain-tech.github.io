feb225a1de2932ad350bd4f9cd0f0024b3d33f4d
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-24 12:11:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-const { keyword = "" } = document.getQueryParameters();
+const {keyword = ""} = document.getQueryParameters();
 let ref = database.ref("article");
 keyword === "" ? ref = ref.limitToLast(4) : YFoSPTA3.innerHTML += `<li class = "q6kpK1jH"><a class = "TnBfB6P0" href = "/?keyword=${keyword}"><span>検索結果: ${keyword}</span></a></li>`
 ref.once("value", snapshot => {
