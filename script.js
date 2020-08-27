65d09b4ca96b8db11e3b6b66ce3d6fc11d190e9b
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-27 22:15:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ keyword === ''
   ? (all === '' && (ref = ref.limitToLast(4)))
   : YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/?keyword=${keyword}'><span>検索結果: ${keyword}</span></a></li>`;
 ref.once('value', snapshot => {
-	const val = snapshot.val() ?? {};
-	Object.keys(val)
+  const val = snapshot.val() ?? {};
+  Object.keys(val)
       .filter(key => (keyword === '' || String(val[key].title).includes(keyword)))
       .forEach(createListItem, val);
-	dismissDialog();
+  dismissDialog();
 }).catch(console.error);
