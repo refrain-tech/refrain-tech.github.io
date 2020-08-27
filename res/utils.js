271935907c573555593676ad8cfeb026bb5f4688
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-27 22:07:20 +0900

    Update utils.js

--- res/utils.js
+++ res/utils.js
@@ -1,45 +1,46 @@
-"use strict";
-Array.prototype.random = function() {
-	return this[(Math.random() * this.length) | 0];
+'use strict';
+Array.prototype.random = function () {
+  return this[(Math.random() * this.length) | 0];
 };
-Array.prototype.shuffle = function() {
-	let index = this.length;
-	while (index > 0) {
-		const random = (Math.random() * index --) | 0;
-		[this[index], this[random]] = [this[random], this[index]];
-	}
-	return this;
+Array.prototype.shuffle = function () {
+  let index = this.length, random;
+  while (index > 0) {
+    random = (Math.random() * index --) | 0;
+    [ this[index], this[random] ] = [ this[random], this[index] ];
+  }
+  return this;
 };
-HTMLElement.prototype.copy = function() {
-	const range = document.createRange();
-	range.selectNode(this);
-	const selection = window.getSelection();
-	selection.removeAllRanges();
-	selection.addRange(range);
-	document.execCommand("copy");
-	selection.removeAllRanges();
+HTMLElement.prototype.copy = function () {
+  const range = document.createRange();
+  range.selectNode(this);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy');
+  selection.removeAllRanges();
 };
-HTMLElement.prototype.prependChild = function(element) {
-	this.insertBefore(element, this.firstChild);
+HTMLElement.prototype.prependChild = function (element) {
+  this.insertBefore(element, this.firstChild);
 };
-HTMLSelectElement.prototype.getSelection = function() {
-	return this.options[this.selectedIndex];
+HTMLSelectElement.prototype.getSelection = function () {
+  return this.options[this.selectedIndex];
 };
-String.prototype.replaceAll = function(regexp, newSubstr) {
-	return this.replace(new RegExp(regexp, "g"), newSubstr);
+String.prototype.replaceAll = function (regexp, newSubstr) {
+  return this.replace(new RegExp(regexp, 'g'), newSubstr);
 };
-document.getQueryParameters = () => location.search.slice(1).split("&").filter(currentValue => /.+=.+/.test(currentValue)).map(currentValue => currentValue.split("=")).reduce((accumulator, currentValue) => {
-	accumulator[currentValue[0]] = currentValue[1];
-	return accumulator;
+document.getQueryParameters = () => location.search.slice(1).split('&').filter(currentValue => /.+=.+/.test(currentValue)).map(currentValue => currentValue.split('=')).reduce((accumulator, currentValue) => {
+  const { key, value } = currentValue;
+  accumulator[key] = value || key;
+  return accumulator;
 }, {});
 Math.average = (...values) => {
-	values = values.filter(isNumber);
-	return Math.sum.apply(null, values) / values.length;
+  values = values.filter(isNumber);
+  return Math.sum.apply(null, values) / values.length;
 };
 Math.median = (...values) => {
-	values = values.filter(isNumber);
-	const index = Math.floor(values.length / 2);
-	return values.length % 2 ? values[index] : (values[index - 1] + values[index]) / 2;
+  values = values.filter(isNumber);
+  const index = Math.floor(values.length / 2);
+  return values.length % 2 ? values[index] : (values[index - 1] + values[index]) / 2;
 };
 Math.sum = (...values) => values.filter(isNumber).reduce((accumulator, currentValue) => accumulator + currentValue);
-window.isNumber = value => typeof value === "number" && isFinite(value);
+window.isNumber = value => typeof value === 'number' && isFinite(value);
