e4107e76c6f78e38bd95f4b5b7086f5f5dae4287
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-28 02:22:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 'use strict';
 const { all = '', keyword = '' } = document.getQueryParameters();
 let ref = database.ref('article');
-keyword === ''
-  ? (all === '' && (ref = ref.limitToLast(4)))
-  : YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/?keyword=${keyword}'><span>検索結果: ${keyword}</span></a></li>`;
+keyword === '' ? all === '' && (ref = ref.limitToLast(4)) : YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/?keyword=${keyword}'><span>検索結果: ${keyword}</span></a></li>`;
 ref.once('value', snapshot => {
   const val = snapshot.val() ?? {};
   Object.keys(val).filter(key => (keyword === '' || String(val[key].title).includes(keyword))).forEach(createListItem, val);
