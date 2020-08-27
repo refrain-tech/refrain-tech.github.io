e9a072aa3b2171b50285aab6a3403c68dc174fb8
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-27 22:04:20 +0900

    Update common.js

--- res/common.js
+++ res/common.js
@@ -1,24 +1,24 @@
-"use strict";
-function createListItem(key) {
-	const {category = "other", image = "/res/favicon.png", title = "", url = ""} = this[key];
-	c6JO6k62.innerHTML += `<section class = "W1JhWxuv"><a category = ${category} class = "TVOxHpZ9" href = ${url === "" ? `/article/?category=${category}&key=${key}` : url} target = "_new"><h1 class = "eFcynxxU">${title}</h1><img class = "WaWu7Va9" src = ${image} /></a></section>`;
+'use strict';
+function createListItem (key) {
+  const { category = 'other', image = '/res/favicon.png', title = '', url = '' } = this[key];
+  c6JO6k62.innerHTML += `<section class = 'W1JhWxuv'><a category = ${category} class = 'TVOxHpZ9' href = ${url === '' ? `/article/?category=${category}&key=${key}` : url} target = '_new'><h1 class = 'eFcynxxU'>${title}</h1><img class = 'WaWu7Va9' src = ${image} /></a></section>`;
 }
-function dismissDialog() {
-	MsDGo7Eg.style.display = "none";
-	m7eOG5wP.style.animation = "none";
+function dismissDialog () {
+  MsDGo7Eg.style.display = 'none';
+  m7eOG5wP.style.animation = 'none';
 }
-mWYXHFlu.addEventListener("keydown", event => {
-	switch (event.keyCode) {
-		case 13:
-			location.href = `/?keyword=${mWYXHFlu.value}`;
-			event.preventDefault();
-			break;
-	}
+mWYXHFlu.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 13:
+      location.href = `/?keyword=${mWYXHFlu.value}`;
+      event.preventDefault();
+      break;
+  }
 });
-SYaLdKxJ.addEventListener("click", event => location.href = `/?keyword=${mWYXHFlu.value}`);
+SYaLdKxJ.addEventListener('click', event => location.href = `/?keyword=${mWYXHFlu.value}`);
 const categories = {
-	article: "記事",
-	content: "コンテンツ",
-	service: "サービス",
-	other: "その他"
+  article: '記事',
+  content: 'コンテンツ',
+  service: 'サービス',
+  other: 'その他'
 };
