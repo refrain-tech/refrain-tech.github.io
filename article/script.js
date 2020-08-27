4d590702e9b325dd415c879c041214b74e2dc076
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-27 22:45:03 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -1,7 +1,7 @@
 'use strict';
 const { category = '', key = '' } = document.getQueryParameters();
 if (!(category in categories)) location.href = '/';
-YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/article/?category=${category}'>${categories[category]}</a></li>`;
+YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/article?category=${category}'>${categories[category]}</a></li>`;
 const ref = key === ''
   ? database.ref('article').orderByChild('category').equalTo(category).limitToLast(4)
   : database.ref(`article/${key}`);
@@ -13,7 +13,7 @@ ref.once('value', snapshot => {
     if (content === '' || title === '') location.href = '/';
     else {
       document.title = title;
-      YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/article/?category=${category}&key=${key}'>${title}</a></li>`;
+      YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/article?category=${category}&key=${key}'>${title}</a></li>`;
       FE9UGRQR.innerHTML = `<section id = 'WSeaG6gm'>${content}</section>`;
     }
   }
